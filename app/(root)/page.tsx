92bbe469ead0a7ee6af1317c8eb7cf3e08e3e34e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-08 09:45:10 +0000

    🏠 Add header component to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
+import HeaderBox from '@/components/headerBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: "John"}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access your account information, transfer funds, and more."
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
