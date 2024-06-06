84472c946d140ff6b00a38c67c18292e262f422e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-06 12:07:42 +0000

    chore: Update TotalBalanceBox component to use a div instead of a paragraph for total balance amount

--- components/totalBalanceBox.tsx
+++ components/totalBalanceBox.tsx
@@ -18,9 +18,9 @@ const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: Tot
           <p className='total-balance-label'>
             Total Current Balance:
           </p>
-          <p className='total-balance-amount flex-center gap-2'>
+          <div className='total-balance-amount flex-center gap-2'>
             <AnimatedCounter amount={totalCurrentBalance} />
-          </p>
+          </div>
         </div>
       </div>
     </section>
