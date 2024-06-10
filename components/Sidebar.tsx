baec487d71fb0fc97083b871290aff59ad01cb8a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-10 13:13:26 +0000

    refactor: Update Sidebar component styles in Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
   return (
     <section className="sidebar">
-      <nav className="flex flex-col gap-4">
+      <nav className="flex flex-col gap-4 w-full">
         <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image src="/icons/logo.svg" width={32} height={32} alt="Horizon Logo" className="size-[24px] max-xl:size-14" />
           <span className="sidebar-logo">BB Bank</span>
@@ -20,9 +20,9 @@ const Sidebar = ({ user }: SiderbarProps) => {
           const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`)
           return (
             <Link href={link.route} key={link.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}>
-              <div className="relative size-8 flex items-center gap-2">
+              <div className="relative size-full flex items-center gap-2">
                 <Image src={link.imgURL} width={24} height={24} alt={link.label} className={cn({'brightness-[3] invert-0' : isActive})} />
-                <p className={cn('text-black-1 text-sm', {'!text-white': isActive})}>{link.label}</p>
+                <p className={cn('text-black-1 text-sm truncate', {'!text-white': isActive})}>{link.label}</p>
               </div>
             </Link>
           )})}
