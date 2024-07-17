dce1a38e130c4d7820a5fb2e823cffdfe25f6888
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-17 10:44:42 +0000

    refactor: Update sign-up form fields and error handling in AuthForm component

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,11 +1,43 @@
-'use server';
+"use server";
 
-export const signIn = async (data: signInProps) => {}
+import { cookies } from "next/headers";
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { parseStringify } from "../utils";
+
+export const signIn = async (data: signInProps) => {};
 
 export const signUp = async (data: SignUpParams) => {
+  const { email, password, firstName, lastName } = data;
   try {
-    
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    return parseStringify(newUserAccount);
   } catch (error) {
-    console.log('Error', error);
+    console.log("Error", error);
+  }
+};
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    console.log("Error", error);
+    return null;
   }
 }
